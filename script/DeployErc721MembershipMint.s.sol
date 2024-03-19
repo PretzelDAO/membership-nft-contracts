ba0f65730527c1c16d0e4426f85bb1ee5d083a49
Christian Ziegler <c@ziegler.dev>
2024-03-19 20:57:17 +0100

    update script with correct checksum vals

--- script/DeployErc721MembershipMint.s.sol
+++ script/DeployErc721MembershipMint.s.sol
@@ -14,8 +14,15 @@ contract DeployErc721MembershipMint is Script {
     HelperConfig public helperConfig;
 
     function run() external returns (Erc721MembershipMint) {
-        helperConfig = new HelperConfig();
-        (, string memory uri, address paymentTokenContractAddress, uint256 paymentTokenContractDecimals, address treasury, string memory defaultImageUrl, string memory defaultMemberRole, address backupAdmin) = helperConfig.activeNetworkConfig();
+
+        string memory uri = "";
+        address paymentTokenContractAddress = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+        uint256 paymentTokenContractDecimals = 6;
+        address treasury = 0xeA46ef9c1B0B6D36bF523758DAbcb1D11B8B4A7B;
+        string memory defaultImageUrl = "ipfs://QmZwd45382Q7BmwguuvskuaT3oeF9Eq6AZ8wq4qCWLdLcC";
+        string memory defaultMemberRole = "Member";
+        address backupAdmin = 0xb1845e478555bfcb183DD9cB748a20e0E3684509;
+
         vm.startBroadcast();
         Erc721MembershipMint erc721MembershipMint = new Erc721MembershipMint(
                 _name,
